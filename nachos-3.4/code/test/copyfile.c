bcd5dc9758a3b426e582430143643e08f6d1bdf6
phuocpr1998 <phuocpr1998@gmail.com>
2018-10-27 22:40:43 +0700

    fix copy file

--- nachos-3.4/code/test/copyfile.c
+++ nachos-3.4/code/test/copyfile.c
@@ -6,53 +6,52 @@
 int main()
 {
 	OpenFileId idSrc, idDes;
-	int src, des, bytesRead = 0;
-	char *fileNameDes, *fileNameSrc;
+	int bytesRead = 0;
+	char *buffer;
 	char *temp;
+	
+
+	// lay ten file va mo file
 	writeFile("Nhap ten file nguon: ", 21, ConsoleInput);
-	src = readFile(fileNameSrc, MAXLENGHT, ConsoleOutput);
+	if (readFile(buffer, MAXLENGHT, ConsoleOutput) <= 0)
+	{
+		writeFile("Lay file nguon that bai\n", 25, ConsoleInput);
+		return 0;
+	}
+	idSrc = openFile(buffer, ReadWrite);
+
 	writeFile("Nhap ten file dich: ", 20, ConsoleInput);
-	des = readFile(fileNameDes, MAXLENGHT, ConsoleOutput);
+	if (readFile(buffer, MAXLENGHT, ConsoleOutput) <= 0)
+	{
+		writeFile("Lay file dich that bai\n", 24, ConsoleInput);
+		return 0;
+	}
+	idDes = openFile(buffer, ReadWrite);
 
-	writeFile(fileNameSrc, MAXLENGHT, ConsoleInput);
-	writeFile(fileNameDes, MAXLENGHT, ConsoleInput);
+	// copy file
+	if (idSrc > 1 && idDes > 1)
+	{
+		do 
+		{
+			bytesRead = readFile(temp, MAXBUFFER, idSrc);
+			if (bytesRead > 0) 
+			{
+				writeFile((char*)temp, bytesRead, idDes);
+			}
+		} while (bytesRead > 0);
 
-	if ((src != -1) && (des != -1))
-	{//sao no y chang z
-		idSrc = openFile(fileNameSrc, ReadWrite);
-		idDes = openFile(fileNameDes, ReadWrite);
-		if (idSrc > 1 && idDes > 1) {
-			do {
-				bytesRead = readFile(temp, MAXBUFFER, idSrc);
-				printf((char*)bytesRead);
-				if (bytesRead > 0) {
-					writeFile((char*)temp, bytesRead, idDes);
-				}
-			} while (bytesRead > 0);
-			
-			//writeFile("Copy file thanh cong.", 21, ConsoleInput);
-			closeFile(idDes);
-			closeFile(idSrc);
-		}
-		else {
-			writeFile("Mo file that bai.", 17, ConsoleInput);
-			if (idDes > 0)
-				closeFile(idDes);
-			if (idSrc > 0)
-				closeFile(idSrc);
-		}
+		writeFile("Copy file thanh cong\n", 22, ConsoleInput);
+		closeFile(idDes);
+		closeFile(idSrc);
 	}
 	else
 	{
-		writeFile("Lay ten file that bai.", 22, ConsoleInput);
+		writeFile("Mo file that bai.\n", 17, ConsoleInput);
+		if (idDes > 0)
+			closeFile(idDes);
+		if (idSrc > 0)
+			closeFile(idSrc);
 	}
-
-	/*if (fileNameDes) {
-		delete[] fileNameDes;
-	}
-	if (fileNameSrc) {
-		delete[] fileNameSrc;
-	}*/
 
 	return 0;
 }
