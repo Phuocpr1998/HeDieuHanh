0d70ea20ae291b042ed495ffdad12185be97b86e
phuocpr1998 <phuocpr1998@gmail.com>
2018-11-21 22:24:59 +0700

    Update PageTableManage

---
+++ Source/nachos-3.4/code/userprog/PageTableManage.cc
@@ -0,0 +1,27 @@
+#include "PageTableManage.h"
+#include "bitmap.h"
+#include "system.h"
+
+PageTableManage::PageTableManage()
+{
+	pageTable = new BitMap(PageSize);
+}
+
+int PageTableManage::FindFreeSlot()
+{
+	return pageTable->Find();
+}
+
+void PageTableManage::Free(int index)
+{
+	pageTable->Clear(index);
+}
+
+bool PageTableManage::Add(int index)
+{
+	if (pageTable->Test(index))
+		return false;
+
+	pageTable->Mark(index);
+	return true;
+}
