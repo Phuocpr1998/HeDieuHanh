4b4622b9f21c75797a6c2ae77b824a44e33c9e9d
Châu Hoàng Phúc <chauhoangphuc432@gmail.com>
2019-01-10 20:50:22 +0700

    fix testShell update

--- Source/nachos-3.4/code/test/testShell.c
+++ Source/nachos-3.4/code/test/testShell.c
@@ -1,48 +1,42 @@
 #include "syscall.h"
-#include "copyright.h"
-
-#define MAXLEN 32
-
-
-int main()
-{
+#include "copyright.h"
+#define MAXLEN 32
+int main()
+{
 	char command[MAXLEN + 1];
-	OpenFileId file; 
+	OpenFileId file;
 	int threadId, byteReaded;
-	int i = 0;
-	do
+	int i = 0;
+	do
 	{
-		Write("~: ",4, ConsoleInput);
+		Write("~: ", 4, ConsoleInput);
 		byteReaded = Read(command, MAXLEN, ConsoleOutput);
-		if(byteReaded == -1){
+		if (byteReaded == -1) {
 			Write("Error!\n", 8, ConsoleInput);
 		}
-		else{
-			if(StrCmp(command,"exit") == 0){
+		else {
+			if (StrCmp(command, "exit") == 0) {
 				break;
-			}
-			else{
-				file = Open(command,ReadOnly);
-				if(file < 0){
-					Write("Ten file loi\n",15, ConsoleInput);
+			}
+			else {
+				file = Open(command, ReadOnly);
+				if (file < 0) {
+					Write("Ten file loi\n", 15, ConsoleInput);
 				}
-				else{
+				else {
 					CloseFile(file);
-
 					threadId = Exec(command);
-					Join(threadId);
-
-					Write("\n",1 , ConsoleInput);
+					if (threadId >= 0) {
+						Join(threadId);
+						Write("\n", 1, ConsoleInput);
+					}					else {
+						Write("Het vung nho\n", 15, ConsoleInput);
+						break;
+					}
 				}
 				command[0] = '\0';
-
-				/*for (; i < byteReaded; ++i)
-				{
-					command[i] = 0;
-				}*/
 			}
-
 		}
-
-	}while(1);
+	} while (1);
+	return 0;
 }
